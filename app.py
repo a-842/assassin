d0935527e456e24c77f8ece5aa230d627521a221
a-842 <131918994+a-842@users.noreply.github.com>
2025-02-13 00:40:20 +0000

    change oprt

--- app.py
+++ app.py
@@ -279,4 +279,4 @@ if __name__ == '__main__':
             db.session.commit()
             print("Admin account created (username: admin, password: adminpass)")
 
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port="5001")
